95bd84a2f0c83484fc5968bd34912669527e259d
Devawayan <devawayanhore92460@gmail.com>
2024-06-19 01:43:41 +0530

    Trust-Fin

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyBanks = () => {
+  return (
+    <div>
+      Bank page
+    </div>
+  )
+}
+
+export default MyBanks
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+
+  const loggedIn = { 
+    firstName: 'Devawayan' , 
+    lastName: 'Hore',
+    email: 'devOwner@gmail.com'
+  };
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTION
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1235.25 }, { currentBalance: 1232.25 }]}
+      />
+
+    </section>
   )
 }
 
